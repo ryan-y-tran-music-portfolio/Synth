b54d741730afc5db682a5ccaddca706c2287f621
Ryan <ryanthetran@gmail.com>
2026-05-26 18:44:13 -0700

    implementing midi_note_to_frequency

--- synth.py
+++ synth.py
@@ -12,15 +12,18 @@ INCREASE_DURING_ATTACK = 1.0 / ATTACK_SAMPLES
 RELEASE_SAMPLES = RAMP_DOWN * SAMPLE_RATE
 DECREASE_DURING_RELEASE = 1.0 / ATTACK_SAMPLES
 
-def midi_note_to_frequency(midi_value: int) -> float:
+def midi_note_to_frequency(base_frequency: float, midi_note: int) -> float:
     """Take Midi Note and convert it to a frequency
     
     Args:
+        base_frequency (float): Base Frequency in Hz
         midi_note (int): Midi Note
 
     Returns:
         Frequency in Hz
     """
+    return base_frequency * (2 ** ((midi_note - 69) / 12))
 
 if __name__ == "__main__":
-    print("Hello")
+    result = midi_note_to_frequency(440.0, 119)
+    print(result)
