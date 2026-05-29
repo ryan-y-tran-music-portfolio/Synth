1ae53865a2d10e5ce0ce2f3bb8c71b7975d6d4ab
Ryan <ryanthetran@gmail.com>
2026-05-28 22:38:20 -0700

    implementing synthesizer_callback (+fixing keyboard interrupt)

--- synth.py
+++ synth.py
@@ -2,38 +2,77 @@ import sounddevice as sd
 import mido
 import numpy
 import questionary
+import time
 
-AMPLITUDE = 0.708 # Full Scale
-RAMP_UP = 0.010 # Milliseconds
-RAMP_DOWN = 0.010 # Milliseconds
+AMPLITUDE = 0.708  # Full Scale
+RAMP_UP = 0.010  # Milliseconds
+RAMP_DOWN = 0.010  # Milliseconds
 SAMPLE_RATE = 48000
-BLOCK_SIZE = 512
+BLOCK_SIZE = 256
 
 ATTACK_SAMPLES = RAMP_UP * SAMPLE_RATE
 INCREASE_DURING_ATTACK = 1.0 / ATTACK_SAMPLES
 
 RELEASE_SAMPLES = RAMP_DOWN * SAMPLE_RATE
-DECREASE_DURING_RELEASE = 1.0 / ATTACK_SAMPLES
+DECREASE_DURING_RELEASE = 1.0 / RELEASE_SAMPLES
+
 
 class Synthesizer:
     def __init__(self):
         """Initializor for Synthesizer"""
-        self.phase = 0.0 # For Sawtooth
+        self.phase = 0.0  # For Sawtooth
         self.synthesizer_amplitude = 0.0
-        self.base_frequency = 440.0 # Edit to whatever
+        self.base_frequency = 440.0  # Edit to whatever
         self.current_frequency = 440.0
-        self.state = 'IDLE' # IDLE (No Press), ATTACK, RELEASE, SUSTAIN
-        self.midi_note = None # Current MIDI note
+        self.state = "IDLE"  # IDLE (No Press), ATTACK, RELEASE, SUSTAIN
+        self.midi_note = None  # Current MIDI note
 
-    def synthesizer_callback(self, outdata: numpy.ndarray, frames: int, time, status: sd.CallbackFlags) -> None:
+    def synthesizer_callback(
+        self, outdata: numpy.ndarray, frames: int, time, status: sd.CallbackFlags
+    ) -> None:
         """Callback for SoundDevice
-        
+
         Args:
             outdata (numpy.ndarray): Where Outgoing audio samples will be written to
             frames (int): Frames to be processed
             time (cffi.CData): Timestamp Info
             status (sd.CallbackFlags): Stream Status Flags
         """
+        phase_increment = self.current_frequency / SAMPLE_RATE
+
+        for i in range(frames):
+            # First, update envelope amplitude
+            if self.state == "ATTACK":
+                self.synthesizer_amplitude += INCREASE_DURING_ATTACK
+                if self.synthesizer_amplitude >= 1.0:
+                    self.synthesizer_amplitude = 1.0
+                    self.state = "SUSTAIN"
+
+            elif self.state == "RELEASE":
+                self.synthesizer_amplitude -= DECREASE_DURING_RELEASE
+                if self.synthesizer_amplitude <= 0.0:
+                    self.synthesizer_amplitude = 0.0
+                    self.state = "IDLE"
+
+            elif self.state == "IDLE":
+                self.synthesizer_amplitude = 0.0
+
+            elif self.state == "SUSTAIN":
+                self.synthesizer_amplitude = 1.0
+
+            # Then generate Sawtooth Sample
+            if self.state == "IDLE":
+                sample = 0.0  # Wave at the moment
+            else:
+                sample = (2.0 * self.phase) - 1.0  # Phase to Audio Range
+                self.phase += phase_increment
+                if self.phase >= 1.0:
+                    self.phase = -1.0  # Keep in range
+
+            outdata[i, 0] = (
+                AMPLITUDE * self.synthesizer_amplitude * sample * 0.2
+            )  # Last number scales the master volume
+
 
 def start_synthesizer(midi_port: str) -> None:
     """Start the Synthesizer, with given midi port.
@@ -43,26 +82,40 @@ def start_synthesizer(midi_port: str) -> None:
     """
 
     print(f"Initializing synthesizer with input port: {midi_port}")
-    synthesizer = Synthesizer() # Initialize Synthesizer
+    synthesizer = Synthesizer()  # Initialize Synthesizer
 
-    output_stream = sd.OutputStream(samplerate=SAMPLE_RATE, blocksize=BLOCK_SIZE, channels=1, callback=synthesizer.synthesizer_callback)
+    output_stream = sd.OutputStream(
+        samplerate=SAMPLE_RATE,
+        blocksize=BLOCK_SIZE,
+        channels=1,
+        callback=synthesizer.synthesizer_callback,
+        latency="low",
+    )
 
     with output_stream:
         try:
             with mido.open_input(midi_port) as inport:
-                for msg in inport:
-                    if msg.type == 'note_on' and msg.velocity > 0:
-                        synthesizer.midi_note = msg.note
-                        synthesizer.current_frequency = midi_note_to_frequency(synthesizer.base_frequency, synthesizer.midi_note)
-                        synthesizer.state = 'ATTACK'
-                    elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
-                        synthesizer.state = 'RELEASE'
+                while True:
+                    for msg in inport.iter_pending():
+                        if msg.type == "note_on" and msg.velocity > 0:
+                            synthesizer.midi_note = msg.note
+                            synthesizer.current_frequency = midi_note_to_frequency(
+                                synthesizer.base_frequency, synthesizer.midi_note
+                            )
+                            synthesizer.state = "ATTACK"
+                        elif msg.type == "note_off" or (
+                            msg.type == "note_on" and msg.velocity == 0
+                        ):
+                            if msg.note == synthesizer.midi_note:
+                                synthesizer.state = "RELEASE"
+                    time.sleep(0.00001)
         except KeyboardInterrupt:
             print("Keyboard exception. Shutting down.")
 
+
 def get_midi_port() -> str:
     """Get MIDI Port, whether it's virtual or physical.
-    
+
     Returns:
         Name of MIDI Port
     """
@@ -74,12 +127,13 @@ def get_midi_port() -> str:
     midi_ports = mido.get_input_names()
     if not midi_ports:
         raise Exception("There were no ports found, therefore the program cannot run.")
-    
+
     return questionary.select("Choose MIDI Input.", choices=midi_ports).ask()
+
 
 def midi_note_to_frequency(base_frequency: float, midi_note: int) -> float:
     """Take Midi Note and convert it to a frequency
-    
+
     Args:
         base_frequency (float): Base Frequency in Hz
         midi_note (int): Midi Note
@@ -88,6 +142,7 @@ def midi_note_to_frequency(base_frequency: float, midi_note: int) -> float:
         Frequency in Hz
     """
     return base_frequency * (2 ** ((midi_note - 69) / 12))
+
 
 if __name__ == "__main__":
     midi_port = get_midi_port()
